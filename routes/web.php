d32bf3e3e52d5a433b46ad93d6476f3ea1c812a0
Thu Hung <tranvothuhung@gmail.com>
2026-03-18 18:54:40 +0700

    Day toan bo route len main

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,13 @@ Route::get('/the-loai-phim', function () {
     
     return view('genres', compact('genres')); 
 });
+
+Route::get('/hop/canada-movies', function () {
+    $movies = DB::table('movie')->where('country_name', 'Canada')->get();
+    return view('canada_movies', ['movies' => $movies]);
+});
+
+Route::get('/phim-tren-120-phut', [MovieController::class, 'phimThoiLuongDai']);
+
+Route::get('/top-movies', [MovieController::class, 'topMovies']);
+
